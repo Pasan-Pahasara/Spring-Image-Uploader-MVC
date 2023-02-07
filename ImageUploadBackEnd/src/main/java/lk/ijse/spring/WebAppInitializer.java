f5539e48d2c5b8a299e4735e9130cd8f37752007
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-08 00:49:05 +0530

    Added WebAppInitializer

--- ImageUploadBackEnd/src/main/java/lk/ijse/spring/WebAppInitializer.java
+++ ImageUploadBackEnd/src/main/java/lk/ijse/spring/WebAppInitializer.java
@@ -1,5 +1,11 @@
 package lk.ijse.spring;
 
+import lk.ijse.spring.config.WebAppConfig;
+import lk.ijse.spring.config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
 
 /**
  * @author : ShEnUx
@@ -7,6 +13,19 @@ package lk.ijse.spring;
  * @date : 2/8/2023
  * @since : 0.1.0
  **/
-public class WebAppInitializer  {
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
 
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
